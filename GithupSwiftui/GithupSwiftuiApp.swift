2e9ec3084b47c072e67fd959c4979abdfa48b378
macos <Phabsokthin123@gmail.com>
2023-09-01 10:41:04 +0700

    Initial Commit

---
+++ GithupSwiftui/GithupSwiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithupSwiftuiApp.swift
+//  GithupSwiftui
+//
+//  Created by MacOS on 1/9/23.
+//
+
+import SwiftUI
+
+@main
+struct GithupSwiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
